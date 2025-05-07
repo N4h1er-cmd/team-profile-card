e5438221ea0f8cb16eb9e6430f7ee6de8ad3785f
N4h1er-cmd <nahiertesfalem@gmail.com>
2025-05-07 11:39:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
+import { defineCustomElements } from "@swisscom/sdx/dist/js/webcomponents/loader";
+import "@swisscom/sdx/dist/css/webcomponents.css";
 import './App.css';
+import image from "./assets/pfp.jpeg"
+
 
 function App() {
+  useEffect(() => {
+    defineCustomElements();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><sdx-header></sdx-header><div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "2rem",
+        backgroundColor: "#f2f4f5"
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          maxWidth: "320px",
+          textAlign: "center"
+        }}
+      >
+
+        <sdx-card label="" style={{ paddingTop: "60px", overflow: "visible" }} image-src={image}>
+          <h2 style={{ marginTop: "1rem", marginBottom: "0.5rem" }}>Hi I'm Nahier!</h2>
+          <p><strong>Software Developer</strong></p>
+          <h3 style={{ marginTop: "1rem", marginBottom: "0.5rem" }}>Facts:</h3>
+          <p> I like to play Basketball.</p>
+          <p> I Read a lot.</p>
+          <p> I like to play Open World games.</p>
+          <sdx-button label="Portfolio" variant="primary" style={{ marginTop: "1rem" }}
+            href="https://nahier.ch"></sdx-button>
+        </sdx-card>
+      </div>
+    </div></>
+    
   );
 }
 
